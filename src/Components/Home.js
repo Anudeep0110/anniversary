b02988f7c68deff0a4df6db8d07057cd4dbb7633
Gude Anudeep <anudeepgude765@gmail.com>
2023-02-15 23:27:12 +0530

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+const Home = () => {
+  return (
+    <>
+       <div className='container p-5 text-white fff' style={{backgroundColor:"rgba(255,255,255,.3)",width:"90vh",height:"90vh",borderRadius:"20px",fontStyle:"italic"}}>
+            <div style={{overflow:"auto"}}>
+              <p className='text-center h1' style={{fontStyle:"normal"}}>Finally 🤍</p>
+              <p className='h4'>Haahhhhh......!!! </p>
+              <p className='h4 ms-5 mt-4'>A very happy and excited anniversary of our love my dear...❤</p>
+              <p className='h4 ms-4 mt-4'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Finally it's Feb 16, the most awaited day on which we both are stepping into another year of our relationship. </p>
+              <p className='h4 ms-4 mt-4'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This is the end of the first year of our relationship that  hides lots  of happiest memories, pains, miss understandings, and many more... 
+              It also teaches us many lessons and make us understand each other better.</p>
+              <p className='h4 ms-4 mt-4'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I always feel like I was the luckiest one ever to be your soulmate because of the love and care that you show on me. And I do always fail in making the same feeling in you. But I promise you that I will do that one day.</p>
+              <p className='h4 ms-4 mt-4'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I promise you that I will be there for you in any situation. And on this day I also promising you that I will support you in every situation and I won't say no for anything that you want to do with or without me. </p>
+              <p className='h4 ms-4 mt-4'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I hope we will be together till the end of our lives and our journey will continue with lots of love and caring and more understanding. </p>
+              <p className='text-center mt-4 h4'>Thank you for being a part of my life amma ❤</p>
+              <p className='text-center mt-3 h4'>I love you so much bangaru ❤</p>
+              <p className='text-center mt-3 h5'>My Amma, Kannamma, Bangaram, Thalli, Pottodu, Pandamma, Sai sree,saiAmma 😘😘😘</p>
+              <p className='text-end mt-5 me-5 h4'>From your love ❤ </p>
+            </div>
+       </div>
+    </>
+  )
+}
+
+export default Home
